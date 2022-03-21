b2913246f35a311719d5ca51d26e01c329b972e5
Fadli Saad <fadlisaad@gmail.com>
2022-03-21 09:46:01 +0800

    Beautify

--- app/Admin/Controllers/UpdateTransactionController.php
+++ app/Admin/Controllers/UpdateTransactionController.php
@@ -17,11 +17,10 @@ class UpdateTransactionController extends AdminController
         $grid = new Grid(new UpdateTransaction());
         $grid->model()->orderBy('id', 'desc');
 
-        $grid->column('id', __('Id'));
+        $grid->column('id', __('ID'));
         $grid->column('payment_type', __('Payment Type'));
-        $grid->column('receipt_no', __('Receipt no'));
+        $grid->column('receipt_no', __('Receipt No.'));
         $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
 
         $grid->filter(function($filter){
             // Remove the default id filter
@@ -46,11 +45,11 @@ class UpdateTransactionController extends AdminController
     {
         $show = new Show(UpdateTransaction::findOrFail($id));
 
-        $show->field('id', __('Id'));
-        $show->field('payment_type', __('Payment type'));
-        $show->field('receipt_no', __('Receipt no'));
-        $show->field('request', __('Request'));
-        $show->field('response', __('Response'));
+        $show->field('id', __('ID'));
+        $show->field('payment_type', __('Payment Type'));
+        $show->field('receipt_no', __('Receipt No.'));
+        $show->field('request', __('Request'))->json();
+        $show->field('response', __('Response'))->json();
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
 
