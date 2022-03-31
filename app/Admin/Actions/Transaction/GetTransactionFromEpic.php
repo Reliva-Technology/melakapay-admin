381ebd78c99ea311f2f99c290422b4979fd77dc9
Fadli Saad <fadlisaad@gmail.com>
2022-03-31 11:04:52 +0800

    exact

--- app/Admin/Actions/Transaction/GetTransactionFromEpic.php
+++ app/Admin/Actions/Transaction/GetTransactionFromEpic.php
@@ -46,7 +46,7 @@ class GetTransactionFromEpic extends RowAction
                         # post data to response page
                         $update = Http::asForm()->post(env('MELAKAPAY_URL').'payment/fpx/response', $data);
 
-                        if($update->body() == 'Successful'){
+                        if($update->body() === 'Successful'){
                             return $this->response()->success('Successfully update transaction ID '.$data['TRANS_ID'])->refresh();
                         } else {
                             return $this->response()->warning('No update required for transaction ID '.$data['TRANS_ID']);
