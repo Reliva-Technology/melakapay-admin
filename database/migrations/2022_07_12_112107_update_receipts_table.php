3a1fc8ecc7b36a09c7f6b2f2619045cb32e43eda
Fadli Saad <fadlisaad@gmail.com>
2022-07-12 11:25:59 +0800

    Modify ic_no column in receipts

---
+++ database/migrations/2022_07_12_112107_update_receipts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateReceiptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('receipts', function (Blueprint $table) {
+            $table->text('ic_no')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
