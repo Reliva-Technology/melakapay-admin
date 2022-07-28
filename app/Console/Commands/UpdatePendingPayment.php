96f9803ca09081b2a773e0726c938bf754a0e0cd
Fadli Saad <fadlisaad@gmail.com>
2022-07-28 17:16:04 +0800

    Add check if exist

--- app/Console/Commands/UpdatePendingPayment.php
+++ app/Console/Commands/UpdatePendingPayment.php
@@ -39,49 +39,54 @@ class UpdatePendingPayment extends Command
                     ->where('merchant_trans_id', $transaction->id)
                     ->first();
 
-                # generate receipt
-                if($epic->receipt_no != NULL)
-                {
-                    $url = env('EPAYMENT_REQUERY_URL').$epic->id;
-                    
-                    $response = Http::get($url);
-                    $response->throw();
+                if($epic){
 
-                    if($response){
+                    # generate receipt
+                    if($epic->receipt_no != NULL)
+                    {
+                        $url = env('EPAYMENT_REQUERY_URL').$epic->id;
+                        
+                        $response = Http::get($url);
+                        $response->throw();
 
-                        $data = json_decode($response->body(),true);
+                        if($response){
 
-                        if(isset($data['STATUS'])){
+                            $data = json_decode($response->body(),true);
 
-                            if($data['STATUS'] == '1'){
+                            if(isset($data['STATUS'])){
 
-                                # post data to response page
-                                if($data['agency'] == 'stom'){
-                                    $result['source'] = 'admin';
-                                    $update = Http::asForm()->post(env('MELAKAPAY_URL').'stom/response', $result);
-                                } else {
-                                    $update = Http::asForm()->post(env('MELAKAPAY_URL').'payment/fpx/response', $result);
+                                if($data['STATUS'] == '1'){
+
+                                    # post data to response page
+                                    if($data['agency'] == 'stom'){
+                                        $result['source'] = 'admin';
+                                        $update = Http::asForm()->post(env('MELAKAPAY_URL').'stom/response', $result);
+                                    } else {
+                                        $update = Http::asForm()->post(env('MELAKAPAY_URL').'payment/fpx/response', $result);
+                                    }
+
+                                    # log pending in DB
+                                    UpdatePayment::updateOrCreate([
+                                        "eps_id" => $epic->id,
+                                        "transaction_id" => $epic->merchant_trans_id,
+                                        "eps_status" => $epic->eps_status,
+                                        "response" => $update->body()
+                                    ]);
+
+                                    Log::info('Save or create log update for EPS ID:'.$epic->id);
+
+                                    sleep(10);
                                 }
-
-                                # log pending in DB
-                                UpdatePayment::updateOrCreate([
-                                    "eps_id" => $epic->id,
-                                    "transaction_id" => $epic->merchant_trans_id,
-                                    "eps_status" => $epic->eps_status,
-                                    "response" => $update->body()
-                                ]);
-
-                                Log::info('Save or create log update for EPS ID:'.$epic->id);
-
-                                sleep(10);
+                                
+                            } else {
+                                Log::info('Status '.$epic->eps_status.' for EPS ID:'.$epic->id);
                             }
-                            
                         } else {
-                            Log::info('Status '.$epic->eps_status.' for EPS ID:'.$epic->id);
+                            Log::info('No response from EPIC.');
                         }
-                    } else {
-                        Log::info('No response from EPIC.');
                     }
+                } else {
+                    Log::info('No matching transaction for ID '.$transactions->id);
                 }
             }
         } else {
